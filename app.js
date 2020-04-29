82f1c8d0b082c7deedeafde6bd396a63e072ac22
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-04-30 02:21:06 +0300

    tune joi

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const validator = require('validator');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 const users = require('./routes/users');
 const articles = require('./routes/articles');
 const { createUser, login } = require('./controllers/users');
+const middleware = require('./middlewares/middleware');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,14 +25,41 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useUnifiedTopology: true,
 });
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.use(requestLogger);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Поле email заполнено некорректно');
+    }),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Поле email заполнено некорректно');
+    }),
+    password: Joi.string().required(),
+  }),
+}), login);
 
 app.use(auth);
-
 app.use('/users', users);
 app.use('/articles', articles);
 
+app.use(middleware);
+app.use(errorLogger);
+
+app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode)
--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
+const validator = require('validator');
+const { celebrate, Joi } = require('celebrate');
 const { createArticle, getArticle, deleteArticle } = require('../controllers/articles');
 
 router.get('/', getArticle);
-router.post('/', createArticle);
-router.delete('/:articleId', deleteArticle);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле link заполнено некорректно');
+    }),
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле image заполнено некорректно');
+    }),
+  }),
+}), createArticle);
+router.delete('/:articleId', celebrate({
+  params: Joi.object().keys({
+    articleId: Joi.string().alphanum().length(24).required(),
+  }),
+}), deleteArticle);
 
 module.exports = router;
