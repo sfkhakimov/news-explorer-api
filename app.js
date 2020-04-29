b4403a813a3a302e0ea9f419632c0a7e91abd1ce
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-04-29 16:01:32 +0300

    create routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const articles = require('./routes/articles');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,6 +15,9 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use('/users', users);
+app.use('/articles', articles);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
