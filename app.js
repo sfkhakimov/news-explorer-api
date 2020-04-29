cebbbbec1a172320a24b14a3978ff038a0070b08
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-04-29 17:29:21 +0300

    Set up routes in app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const users = require('./routes/users');
 const articles = require('./routes/articles');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
@@ -15,6 +19,11 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+
+app.use(auth);
 
 app.use('/users', users);
 app.use('/articles', articles);
